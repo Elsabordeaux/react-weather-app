2de42a5d21b7f91d5376ff69f3f76029582a1849
Elsabordeaux <snowpaws85@gmail.com>
2024-09-12 18:01:26 +0100

    removed axios temporarily to see if issue is resolved

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
-import axios from "axios";
+
 import PacmanLoader from "react-spinners/PacmanLoader";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello from Alana j</h1>
+        <h1>Hello from Alana</h1>
         <PacmanLoader color="fuchsia" />
       </header>
     </div>
