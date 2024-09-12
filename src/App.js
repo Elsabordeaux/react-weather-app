64042d873a3bb040001f2f07fcc641f9020f8c4b
Elsabordeaux <snowpaws85@gmail.com>
2024-09-12 12:10:51 +0100

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import axios from "axios";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from Alana j</h1>
+        <PacmanLoader color="fuchsia" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
